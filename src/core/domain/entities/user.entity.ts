c6d7ea3b1f4ee49fcd86827cb59ffb4ee9326dab
Vinicius Miguel <vini.miguel1994@gmail.com>
2023-01-20 10:27:49 -0300

    refactor: format

--- src/core/domain/entities/user.entity.ts
+++ src/core/domain/entities/user.entity.ts
@@ -1,7 +1,7 @@
-import { Entity } from '@/core/base'
+import { Entity } from '@/core/base';
 
 export interface UserEntity extends Entity {
-  name: string
-  password: string
-  email: string
+  name: string;
+  password: string;
+  email: string;
 }
